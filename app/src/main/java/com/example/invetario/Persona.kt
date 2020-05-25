be1ad8143ff58d6ccebd7acde5339f0852171384
efnaranjo6 <efnaranjo6@misena.edu.co>
2020-05-25 12:05:45 -0500

    actualizacion de las intefaces con firebase

---
+++ app/src/main/java/com/example/invetario/Persona.kt
@@ -0,0 +1,7 @@
+package com.example.invetario
+
+import android.media.Rating
+
+class Persona(val idpvghvhh: String, val nombre: String, val apellido:String, val cedula:String, val correo:String,val contraseña:String ) {
+
+}
--- app/src/main/java/com/example/invetario/RegistroAdmin.kt
+++ app/src/main/java/com/example/invetario/RegistroAdmin.kt
@@ -1,11 +1,15 @@
 package com.example.invetario
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_olvido_admin.*
 import kotlinx.android.synthetic.main.activity_registroadmin.*
 
@@ -39,9 +43,20 @@ class RegistroAdmin : AppCompatActivity() {
         }
 
     }
-    private fun guardarPersona{
+    private fun guardarPersona(){
         val nombre = txtNombre.text.toString().trim()
+        val apellido = txtApellido.text.toString().trim()
+        val cedula = txtCedula.text.toString().trim()
+        val correo = txtCorreo.text.toString().trim()
+        val contraseña = txtNombre.text.toString().trim()
 
+        val ref = FirebaseDatabase.getInstance().getReference("Personas")
+        val personaId = ref.push().key
+        val id = personaId.toString()
+        val persona = Persona(id,nombre,apellido,cedula,correo,contraseña)
+        ref.child(id).setValue(persona).addOnCompleteListener{
+            Toast.makeText(applicationContext, " Persona guardada", Toast.LENGTH_LONG).show()
 
+        }
     }
 }
